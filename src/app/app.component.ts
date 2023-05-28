9b45f9df7db0b75bdc62881959c5db30b49b56f0
MarkedOliv <olivdev4@gmail.com>
2023-05-28 18:38:47 -0400

    feat(frontend): dbz module, list and addCharacter component, understanding services, interfaces and forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: [],
 })
 export class AppComponent {
-  public title: string = 'Mi primera app de Angular';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { CounterModule } from './counter/counter.module';
-import { HeroesModule } from './heroes/heroes.module';
+/* import { CounterModule } from './counter/counter.module';
+import { HeroesModule } from './heroes/heroes.module'; */
 
 import { AppComponent } from './app.component';
+import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, CounterModule, HeroesModule],
+  imports: [BrowserModule, DbzModule, /* CounterModule, HeroesModule */],
   providers: [],
   bootstrap: [AppComponent],
 })
